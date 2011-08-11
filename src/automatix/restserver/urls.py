7efd59c99c435809b604778d3ba6dc46f1f5a3bd
Marco <ilmorna@gmail.com>
2011-08-11 16:32:00 +0200

    Created rest call/answer working with templates Added Widget Dashboard

--- src/automatix/restserver/urls.py
+++ src/automatix/restserver/urls.py
@@ -7,7 +7,14 @@ Created on Aug 10, 2011
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('automatix.restserver.views',
+  #Response without templating
   url(r'^mcollective/(?P<filters>[\w|\W]+)/(?P<agent>[\w|\W]+)/(?P<action>[\w|\W]+)/$', 'get', name = "mcollective-rest-server"),
   url(r'^mcollective/(?P<filters>[\w|\W]+)/(?P<agent>[\w|\W]+)/(?P<action>[\w|\W]+)/(?P<args>[\w|\W]+)/$', 'get', name = "mcollective-rest-server"),
-  url(r'^mcollective/(.*)', 'get'),
+  #Response with template
+  url(r'^mcollective-template/(?P<template>[\w|\W]+)/(?P<filters>[\w|\W]+)/(?P<agent>[\w|\W]+)/(?P<action>[\w|\W]+)/$', 'getWithTemplate', name = "mcollective-rest-server"),
+  url(r'^mcollective-template/(?P<template>[\w|\W]+)/(?P<filters>[\w|\W]+)/(?P<agent>[\w|\W]+)/(?P<action>[\w|\W]+)/(?P<args>[\w|\W]+)/$', 'getWithTemplate', name = "mcollective-rest-server"),
+  
+  #Scripts execution
+  url(r'^script/(?P<language>[\w|\W]+)/(?P<script>[\w|\W]+)/$', 'executeScript', name = "script-execution"),
+  
 )
--- src/automatix/restserver/views.py
+++ src/automatix/restserver/views.py
@@ -6,6 +6,9 @@ Created on Aug 10, 2011
 import httplib2
 from django.http import HttpResponse
 from django.conf import settings
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+import json
 
 def get(request, filters, agent, action, args=None):
     http = httplib2.Http()
@@ -19,5 +22,32 @@ def get(request, filters, agent, action, args=None):
     response, content = http.request(url, "GET")
     if response.status == 200:
         return HttpResponse(content, mimetype="application/json")
+        
     return response
+
+def getWithTemplate(request, template, filters, agent, action, args=None):
+    if request.is_ajax():
+        http = httplib2.Http()
+        url = settings.RUBY_REST_BASE_URL
+        url += filters + "/"
+        url += agent + "/"
+        url += action + "/"
+        if args:
+            url += args + "/"
+            
+        response, content = http.request(url, "GET")
+        if response.status == 200:
+            jsonObj = json.loads(content)
+            templatePath = 'ajax/' + template + '.html'
+            data = {
+                    'content': jsonObj
+            }
+            return render_to_response( templatePath, data,
+                context_instance = RequestContext( request ) )
+    return response
+
+def executeScript(request, language, script):
+    print language
+    print script
+    return ""
 
