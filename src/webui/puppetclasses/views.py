91e54956e719aa8ee600261f62687af9fa765a50
Marco <ilmorna@gmail.com>
2011-08-19 11:34:49 +0200

    Removed puppet class from tree if no server inside

--- src/webui/puppetclasses/views.py
+++ src/webui/puppetclasses/views.py
@@ -10,41 +10,30 @@ class QueryMethods(object):
     
     def get_tree_nodes(self, level, path):
         logger.info("Calling get_tree_nodes for level: " + str(level))
-        #Call for parent
         classes = PuppetClass.objects.filter(enabled=True, level=level+1)
         data = []
-        for puppetclass in classes:
-            content = {"isFolder": "true", "isLazy": "true", "title": puppetclass.name, "level":puppetclass.level, "key":puppetclass.name}
-            data.append(content)
-        
         if path:
-            #We cannot use / inside rest url, so / was substituted by _
-            #Here we revert this change to obtain a correct path
             path = path.replace('_', '/')
-            logger.info("Looking for servers in path: " + path)
-            servers = Server.objects.filter(puppet_path=path)
-            for server in servers:
-                serverdata = {"title":server.hostname, "url": "/server/details/"+server.hostname+"/", "key":server.hostname}
-                data.append(serverdata)
+        else:
+            path = ''
+        for puppetclass in classes:
+            test_path=path+'/'+puppetclass.name
+            server = Server.objects.filter(puppet_path__startswith=test_path)
+            if len(server)>0:
+                content = {"isFolder": "true", "isLazy": "true", "title": puppetclass.name, "level":puppetclass.level, "key":puppetclass.name}
+                data.append(content)
+            else:
+                logger.info("Excluding class " + str(puppetclass) + " because there are no server inside")
+        
+        #We cannot use / inside rest url, so / was substituted by _
+        #Here we revert this change to obtain a correct path
+        logger.info("Looking for servers in path: " + path)
+        servers = Server.objects.filter(puppet_path=path)
+        for server in servers:
+            serverdata = {"title":server.hostname, "url": "/server/details/"+server.hostname+"/", "key":server.hostname}
+            data.append(serverdata)
              
         return json.dumps(data)
-    
-    def get_child_data(self, current_node):
-        childlist = []
-        for child in current_node.children.all():
-            #children = self.get_child_data(child)
-            childata = {"isFolder":"true", "isLazy": "true", "title":child.name, "id":current_node.pk}
-            childlist.append(childata) 
-           
-        if len(childlist) == 0:
-            logger.info("Retrieving servers contained in class " + current_node.name)
-            #Getting server in current class
-            servers = Server.objects.filter(puppet_classes=current_node)
-            for server in servers:
-                childata = {"title":server.hostname, "url": "/server/details/"+server.hostname+"/"}
-                childlist.append(childata) 
-            
-        return childlist
         
 def query(request, operation, level, path=None):
     query_methods = QueryMethods()
