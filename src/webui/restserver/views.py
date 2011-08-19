6de3f4275fa44a8afe2dc7390bc8c2ef4cc84d09
Marco <ilmorna@gmail.com>
2011-08-19 10:22:21 +0200

    Added Refresh Server Inventory in Admin Area

--- src/webui/restserver/views.py
+++ src/webui/restserver/views.py
@@ -11,6 +11,9 @@ from django.template.context import RequestContext
 from django.utils import simplejson as json
 import logging
 from webui.widgets.loading import registry
+from datetime import datetime
+from webui.serverstatus.models import Server, Agent
+from webui.puppetclasses.models import PuppetClass
 
 logger = logging.getLogger(__name__)
 
@@ -54,6 +57,90 @@ def executeAction(request, action):
     actionToExecute()
     return HttpResponse('')
 
+def server_inventory():
+    try: 
+        response, content = callRestServer('no-filter', 'nodeinfo', 'basicinfo')
+        if response.status == 200:
+            jsonObj = json.loads(content)
+            update_time = datetime.now()
+            for server in jsonObj:
+                #verify if server exists in database
+                server_name = server['sender']
+                #Using filter because get sometimes generates query error
+                #The filter result is a collection, so you need to select the first element to work
+                query_response = Server.objects.filter(hostname=server_name)
+                if query_response:
+                    retrieved_server = query_response[0]
+                    logger.info("Updating Server information " + server_name)
+                    retrieved_server.online = True
+                    retrieved_server.os = server['data']['facts']['lsbdistdescription']
+                    retrieved_server.architecture = server['data']['facts']['architecture']
+                    retrieved_server.updated_time = update_time
+                    #Add puppet_classes
+                    add_puppet_classes(retrieved_server, server['data']['classes'])
+                    #Add agents
+                    add_agents(retrieved_server, server['data']['agentlist'])
+                    #Create PuppetClass Path
+                    create_path(retrieved_server, server['data']['classes'])
+                    retrieved_server.save()
+                else: 
+                    logger.info("Creating new server with name " + server_name)
+                    new_server = Server.objects.create(hostname=server_name)
+                    print server['data']['facts']['lsbdistdescription']
+                    print server['data']['facts']['architecture']
+                    new_server.os = server['data']['facts']['lsbdistdescription']
+                    new_server.architecture = server['data']['facts']['architecture']
+                    new_server.online = True
+                    new_server.updated_time = update_time
+                    #Add puppet_classes
+                    add_puppet_classes(new_server, server['data']['classes'])
+                    #Add agents
+                    add_agents(new_server, server['data']['agentlist'])
+                    #Create PuppetClass Path
+                    create_path(new_server, server['data']['classes'])
+
+                    new_server.save()
+                
+                #Retrieving not updated/created server and set them to OFFLINE
+                logger.info("Checking offline servers")
+                not_updated = Server.objects.filter(updated_time__lt=update_time)
+                for server in not_updated:
+                    server.online = False
+                    server.save()
+                        
+    except Exception, err:
+        logger.error('ERROR: ' + str(err))
+            
+            
+            
+def add_puppet_classes(server, puppet_classes):
+    for current in puppet_classes:
+        retrieved = PuppetClass.objects.filter(name=current)
+        if retrieved:     
+            server.puppet_classes.add(retrieved[0])
+        else:
+            logger.warn("Cannot find class with name " + current)
+            
+def add_agents(server, agents_list):
+    for agent in agents_list:
+        retrieved = Agent.objects.filter(name=agent)
+        if retrieved:     
+            server.agents.add(retrieved[0])
+        else:
+            logger.info("Discovered new agent " + agent)
+            new_agent = Agent.objects.create(name=agent)
+            server.agents.add(new_agent) 
+
+def create_path(server, puppet_classes):
+    path = ''
+    for i in range (0, 5):
+        level_classes = PuppetClass.objects.filter(level=i).values_list('name', flat=True)
+        extracted_class = iter(set(puppet_classes).intersection(set(level_classes))).next()
+        if extracted_class:
+            path = path + '/' + extracted_class
+    
+    server.puppet_path=path
+
 class Actions(object):
 
     def refresh_dashboard(self):
@@ -64,4 +151,9 @@ class Actions(object):
             for widget in widgets:
                 retrieved = registry.get_widget(widget['name'])
                 retrieved.db_reference = widget
+                
+    def refresh_server_inventory(self):
+        logger.info("Calling Refresh Inventory")
+        server_inventory()
+        
 
--- src/webui/serverstatus/cron.py
+++ src/webui/serverstatus/cron.py
@@ -6,11 +6,7 @@ Created on Aug 16, 2011
 
 from webui.django_cron import cronScheduler, Job
 import logging
-from webui.restserver.views import callRestServer
-from django.utils import simplejson as json
-from webui.serverstatus.models import Server, Agent
-from webui.puppetclasses.models import PuppetClass
-from datetime import datetime
+from webui.restserver.views import server_inventory
 
 logger = logging.getLogger(__name__)
 
@@ -24,88 +20,7 @@ class UpdateServerStatus(Job):
 
         def job(self):
             logger.info("Running Job UpdateServerStatus")
-            try: 
-                response, content = callRestServer('no-filter', 'nodeinfo', 'basicinfo')
-                if response.status == 200:
-                    jsonObj = json.loads(content)
-                    update_time = datetime.now()
-                    for server in jsonObj:
-                        #verify if server exists in database
-                        server_name = server['sender']
-                        #Using filter because get sometimes generates query error
-                        #The filter result is a collection, so you need to select the first element to work
-                        query_response = Server.objects.filter(hostname=server_name)
-                        if query_response:
-                            retrieved_server = query_response[0]
-                            logger.info("Updating Server information " + server_name)
-                            retrieved_server.online = True
-                            retrieved_server.os = server['data']['facts']['lsbdistdescription']
-                            retrieved_server.architecture = server['data']['facts']['architecture']
-                            retrieved_server.updated_time = update_time
-                            #Add puppet_classes
-                            self.add_puppet_classes(retrieved_server, server['data']['classes'])
-                            #Add agents
-                            self.add_agents(retrieved_server, server['data']['agentlist'])
-                            #Create PuppetClass Path
-                            self.create_path(retrieved_server, server['data']['classes'])
-                            retrieved_server.save()
-                        else: 
-                            logger.info("Creating new server with name " + server_name)
-                            new_server = Server.objects.create(hostname=server_name)
-                            print server['data']['facts']['lsbdistdescription']
-                            print server['data']['facts']['architecture']
-                            new_server.os = server['data']['facts']['lsbdistdescription']
-                            new_server.architecture = server['data']['facts']['architecture']
-                            new_server.online = True
-                            new_server.updated_time = update_time
-                            #Add puppet_classes
-                            self.add_puppet_classes(new_server, server['data']['classes'])
-                            #Add agents
-                            self.add_agents(new_server, server['data']['agentlist'])
-                            #Create PuppetClass Path
-                            self.create_path(new_server, server['data']['classes'])
-
-                            new_server.save()
-                        
-                        #Retrieving not updated/created server and set them to OFFLINE
-                        logger.info("Checking offline servers")
-                        not_updated = Server.objects.filter(updated_time__lt=update_time)
-                        for server in not_updated:
-                            server.online = False
-                            server.save()
-                       
-            except Exception, err:
-                logger.error('ERROR: ' + str(err))
-                
-                
-                
-        def add_puppet_classes(self, server, puppet_classes):
-            for current in puppet_classes:
-                retrieved = PuppetClass.objects.filter(name=current)
-                if retrieved:     
-                    server.puppet_classes.add(retrieved[0])
-                else:
-                    logger.warn("Cannot find class with name " + current)
-                    
-        def add_agents(self, server, agents_list):
-            for agent in agents_list:
-                retrieved = Agent.objects.filter(name=agent)
-                if retrieved:     
-                    server.agents.add(retrieved[0])
-                else:
-                    logger.info("Discovered new agent " + agent)
-                    new_agent = Agent.objects.create(name=agent)
-                    server.agents.add(new_agent) 
-        
-        def create_path(self, server, puppet_classes):
-            path = ''
-            for i in range (0, 5):
-                level_classes = PuppetClass.objects.filter(level=i).values_list('name', flat=True)
-                extracted_class = iter(set(puppet_classes).intersection(set(level_classes))).next()
-                if extracted_class:
-                    path = path + '/' + extracted_class
-            
-            server.puppet_path=path
+            server_inventory()
                 
                 
 cronScheduler.register(UpdateServerStatus)
