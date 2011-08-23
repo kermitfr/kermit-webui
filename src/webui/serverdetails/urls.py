13c5bf9d672e3647b49a2ca842c11072191314d1
Marco <ilmorna@gmail.com>
2011-08-23 15:36:48 +0200

    Added server details pages

--- src/webui/serverdetails/urls.py
+++ src/webui/serverdetails/urls.py
@@ -7,9 +7,10 @@ Created on Aug 18, 2011
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('webui.serverdetails.views',
-  url(r'^details/(?P<hostname>[\w|\W]+)/(?P<resource_name>[\w|\W]+)/instance/$', 'instanceInventory', name = "mcollective-inventory"),
-  url(r'^details/(?P<hostname>[\w|\W]+)/(?P<resource_name>[\w|\W]+)/datasources/$', 'datasourceListInventory', name = "mcollective-inventory"),
-  url(r'^details/(?P<hostname>[\w|\W]+)/(?P<resource_name>[\w|\W]+)/datasource/$', 'datasourceInventory', name = "mcollective-inventory"),
+  url(r'^details/(?P<hostname>[\w|\W]+)/(?P<instance_name>[\w|\W]+)/(?P<resource_name>[\w|\W]+)/instance/$', 'instanceInventory', name = "mcollective-inventory"),
+  url(r'^details/(?P<hostname>[\w|\W]+)/(?P<instance_name>[\w|\W]+)/(?P<resource_name>[\w|\W]+)/datasources/$', 'datasourceListInventory', name = "mcollective-inventory"),
+  url(r'^details/(?P<hostname>[\w|\W]+)/(?P<instance_name>[\w|\W]+)/(?P<resource_name>[\w|\W]+)/datasource/$', 'datasourceInventory', name = "mcollective-inventory"),
+  url(r'^details/(?P<hostname>[\w|\W]+)/(?P<instance_name>[\w|\W]+)/(?P<resource_name>[\w|\W]+)/application/$', 'applicationInventory', name = "mcollective-inventory"),
   #Server Inventory
   url(r'^details/(?P<hostname>[\w|\W]+)/$', 'hostInventory', name = "mcollective-inventory"),
   url(r'^tree/(?P<hostname>[\w|\W]+)/$', 'getDetailsTree', name = "mcollective-inventory"),
--- src/webui/serverdetails/views.py
+++ src/webui/serverdetails/views.py
@@ -14,7 +14,7 @@ def hostInventory(request, hostname):
     server_info = read_server_info(hostname)    
     if not server_info:
         server_info = []
-    return render_to_response('server/details.html', {"base_url": settings.BASE_URL, "serverdetails": server_info, "hostname": hostname}, context_instance=RequestContext(request))
+    return render_to_response('server/details.html', {"base_url": settings.BASE_URL, "static_url":settings.STATIC_URL, "serverdetails": server_info, "hostname": hostname}, context_instance=RequestContext(request))
 
 def getDetailsTree(request, hostname):
     data = []
@@ -26,22 +26,22 @@ def getDetailsTree(request, hostname):
         db_instances = {'title': 'Instances', 'isFolder':"true", "key":"instance", "icon":"app_server.png", "type":"instances"}
         dbs = []
         for instance in server_info:
-            db = {'title':instance['id'], "key":instance['id'], "icon":"web_instance.png", "type":"instance", "detailsEnabled":"true"}
+            db = {'title':instance['id'], "key":instance['id'], "icon":"web_instance.png", "type":"instance", "instance":instance['id'],"detailsEnabled":"true"}
             #Configuring Applications
             logger.debug('Configuring Applications')
             applications = {'title': 'Applications', 'isFolder':"true", "key":"applications", "icon":"folder_applications.png", "type":"applications"}
             apps = []
             for appli in instance['applilist']:
-                app = {'title':appli['name'], "key":appli['name'], "type":"application"}
+                app = {'title':appli['name'], "key":appli['name'], "icon":"application.png", "type":"application", "instance":instance['id'], "detailsEnabled":"true"}
                 apps.append(app)
             applications['children'] = apps
             
             #Configuring Datasources
             logger.debug('Configuring Datasources')
-            datasources = {'title': 'Datasources', 'isFolder':"true", "key":instance['id'], "icon":"folder_database.png", "type":"datasources", "detailsEnabled":"true"}
+            datasources = {'title': 'Datasources', 'isFolder':"true", "key":"datasources", "icon":"folder_database.png", "type":"datasources", "instance":instance['id'], "detailsEnabled":"true"}
             dss = []
             for datasource in instance['datasource']:
-                datasource = {'title':datasource['name'], "key":datasource['name'], "type":"datasource"}
+                datasource = {'title':datasource['name'], "key":datasource['name'], "icon":"datasource.png", "type":"datasource", "instance":instance['id']}
                 dss.append(datasource)
             datasources['children'] = dss
             
@@ -55,44 +55,62 @@ def getDetailsTree(request, hostname):
     data.append(content)
     return HttpResponse(json.dumps(data))
 
-def instanceInventory(request, hostname, resource_name):
+def instanceInventory(request, hostname, instance_name, resource_name):
     server_info = read_server_info(hostname)
     if server_info:
         instance = None 
         for server in server_info:
-            if server['id'] == resource_name:
+            if server['id'] == instance_name:
                 instance = server
                 break 
-        return render_to_response('server/instance.html', {"base_url": settings.BASE_URL, "java_stop_options":instance['java-stop-options'], "java_start_options":instance['java-start-options'], "oc4j_options":instance['oc4j-options'], "hostname": hostname}, context_instance=RequestContext(request))
+        return render_to_response('server/instance.html', {"base_url": settings.BASE_URL, "static_url":settings.STATIC_URL, "java_stop_options":instance['java-stop-options'], "java_start_options":instance['java-start-options'], "oc4j_options":instance['oc4j-options'], "hostname": hostname}, context_instance=RequestContext(request))
     else:
-        return render_to_response('server/instance.html', {"base_url": settings.BASE_URL, "hostname": hostname}, context_instance=RequestContext(request))
+        return render_to_response('server/instance.html', {"base_url": settings.BASE_URL, "static_url":settings.STATIC_URL, "hostname": hostname}, context_instance=RequestContext(request))
 
 
-def datasourceListInventory(request, hostname, resource_name):
+def datasourceListInventory(request, hostname, instance_name, resource_name):
     server_info = read_server_info(hostname)
     if server_info:
         instance = None 
         for server in server_info:
-            if server['id'] == resource_name:
-                instance = server
-                break 
-        for datasource in instance['datasource']:
-            convert_keys_names(datasource)
-        return render_to_response('server/datasource.html', {"base_url": settings.BASE_URL, "datasources": instance['datasource']}, context_instance=RequestContext(request))
-    else:
-        return render_to_response('server/datasource.html', {"base_url": settings.BASE_URL, "hostname": hostname}, context_instance=RequestContext(request))
-
-
-def datasourceInventory(request, hostname, resource_name):
-    server_info = read_server_info(hostname)
-    if server_info:
-        instance = None 
-        for server in server_info:
-            if server['id'] == resource_name:
+            if server['id'] == instance_name:
                 instance = server
                 break 
         for datasource in instance['datasource']:
             convert_keys_names(datasource)
-        return render_to_response('server/datasource.html', {"base_url": settings.BASE_URL, "datasources": instance['datasource']}, context_instance=RequestContext(request))
+        return render_to_response('server/datasource.html', {"base_url": settings.BASE_URL, "static_url":settings.STATIC_URL, "datasources": instance['datasource']}, context_instance=RequestContext(request))
     else:
-        return render_to_response('server/datasource.html', {"base_url": settings.BASE_URL, "hostname": hostname}, context_instance=RequestContext(request))
+        return render_to_response('server/datasource.html', {"base_url": settings.BASE_URL, "static_url":settings.STATIC_URL, "hostname": hostname}, context_instance=RequestContext(request))
+
+
+def datasourceInventory(request, hostname, instance_name, resource_name):
+    server_info = read_server_info(hostname)
+    if server_info:
+        instance = None 
+        for server in server_info:
+            if server['id'] == instance_name:
+                instance = server
+                break 
+        for datasource in instance['datasource']:
+            convert_keys_names(datasource)
+        return render_to_response('server/datasource.html', {"base_url": settings.BASE_URL, "static_url":settings.STATIC_URL, "datasources": instance['datasource']}, context_instance=RequestContext(request))
+    else:
+        return render_to_response('server/datasource.html', {"base_url": settings.BASE_URL, "static_url":settings.STATIC_URL, "hostname": hostname}, context_instance=RequestContext(request))
+
+def applicationInventory(request, hostname, instance_name, resource_name):
+    server_info = read_server_info(hostname)
+    if server_info:
+        instance = None 
+        for server in server_info:
+            if server['id'] == instance_name:
+                instance = server
+                break
+        selected_app = None 
+        for app in instance['applilist']:
+            if app['name'] == resource_name:
+                selected_app = app
+                convert_keys_names(selected_app)
+        return render_to_response('server/application.html', {"base_url": settings.BASE_URL, "static_url":settings.STATIC_URL,"hostname":hostname, "instance_id":instance['id'] ,"application": selected_app}, context_instance=RequestContext(request))
+    else:
+        return render_to_response('server/application.html', {"base_url": settings.BASE_URL, "static_url":settings.STATIC_URL, "hostname": hostname}, context_instance=RequestContext(request))
+
