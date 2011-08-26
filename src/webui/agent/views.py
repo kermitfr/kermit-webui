9477e187cfc65d7b814477468e451fa5ea299263
Marco <ilmorna@gmail.com>
2011-08-26 13:45:16 +0200

    Removed unused admin stuffs Completed default response template for agents

--- src/webui/agent/views.py
+++ src/webui/agent/views.py
@@ -90,8 +90,21 @@ def execute_action_form(request, agent, action, xhr=None):
                         rendered_template = render_to_string(template_name, {'settings':settings, 'content': jsonObj, 'agent': agent, 'action': action, 'arguments':form.cleaned_data})
                         rdict.update({'response': rendered_template, 'type':'html'})
                     except TemplateDoesNotExist, e:
-                        logger.debug('No template found for ' + agent + ' - ' + action + '! Using default JSON viewer')
-                        rdict.update({'response': jsonObj, 'type':'json'})
+                        try:
+                            logger.debug('No template found for ' + agent + ' - ' + action + '! Using default agent template')
+                            template_name = 'agents/default.html'
+                            get_template(template_name)
+                            logger.debug('Getting DDL output information')
+                            outputs = get_action_outputs(agent, action)
+                            if outputs:
+                                rendered_template = render_to_string(template_name, {'settings':settings, 'content': jsonObj, 'outputs':outputs,'agent': agent, 'action': action, 'arguments':form.cleaned_data})
+                                rdict.update({'response': rendered_template, 'type':'html'})
+                            else:
+                                logger.debug('No DDL outputs found for ' + agent + ' ' + action + '. Send default JSON')
+                                rdict.update({'response': jsonObj, 'type':'json'})
+                        except:
+                            logger.debug('No template found for ' + agent + ' - ' + action + '! Using default JSON viewer')
+                            rdict.update({'response': jsonObj, 'type':'json'})
                 
             # Make a json whatsit to send back.
             json_data = json.dumps(rdict, ensure_ascii=False)
@@ -116,4 +129,17 @@ def get_action_inputs(agent, action):
         if action_db:
                 if len(action_db.inputs.values()) > 0:
                     return action_db.inputs.values()
+    return None
+
+def get_action_outputs(agent, action):
+    agent_db = Agent.objects.get(name=agent)
+    if agent_db:
+        action_db = Action.objects.get(name=action, agent=agent_db)
+        if action_db:
+                if len(action_db.outputs.values()) > 0:
+                    output_dict = {}
+                    for out in action_db.outputs.values():
+                        output_dict[out['name']] = out['display_as']
+                        
+                    return output_dict
     return None
