c55ba53dc417bce6093f13e8d64b0dc5db0b30b0
Marco Mornati <ilmorna@gmail.com>
2011-09-20 14:37:35 +0200

    Added back to Kermit link in admin area

--- src/webui/index/urls.py
+++ src/webui/index/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('webui.index.views',
 	url(r'^index/', 'index', name="index"),
 	url(r'^credits/', 'credits', name="credits"),
-	url(r'', 'index'),
+	url(r'', 'index', name="main_kermit"),
 )
 
