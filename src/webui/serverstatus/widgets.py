2bf62c635f6fab0c02b9972d7c6eef780b86e78d
Marco Mornati <ilmorna@gmail.com>
2011-10-25 15:53:36 +0200

    Added Widget ServerStatus With Classes Information Renamed ServerStatus to SystemStatus (Classes and files)

--- src/webui/serverstatus/widgets.py
+++ src/webui/serverstatus/widgets.py
@@ -10,8 +10,8 @@ from guardian.shortcuts import get_objects_for_user
 
 logger = logging.getLogger(__name__)
 
-class DashBoardServerStatus(Widget):
-    template = "widgets/serverstatus/serverstatus.html"
+class SystemStatus(Widget):
+    template = "widgets/serverstatus/systemstatus.html"
     
     def get_context(self):
         super_context = super(self.__class__,self).get_context()
@@ -21,4 +21,36 @@ class DashBoardServerStatus(Widget):
                 servers = get_objects_for_user(self.user, 'use_server', Server)
         
         widget_context = {"servers":servers}
+        return dict(super_context.items() + widget_context.items())
+    
+class ServerStatus(Widget):
+    template = "widgets/serverstatus/serverstatus.html"
+    
+    def get_context(self):
+        super_context = super(self.__class__,self).get_context()
+        servers = Server.objects.filter(deleted=False)
+        if self.user != 'fooUser':
+            if not self.user.is_superuser:
+                servers = get_objects_for_user(self.user, 'use_server', Server)
+        
+        #Extracting puppet classes level
+        serverslist=[]
+        for server in servers:
+            serverdict = {}
+            serverdict["online"]=server.online
+            serverdict["fqdn"]=server.fqdn
+            serverdict["hostname"]=server.hostname
+            for puppetclass in server.puppet_classes.values():
+                if puppetclass["level"]==0:
+                    serverdict["lvl1"]=puppetclass["name"]
+                elif puppetclass["level"]==1:
+                    serverdict["lvl2"]=puppetclass["name"]
+                elif puppetclass["level"]==2:
+                    serverdict["lvl3"]=puppetclass["name"]
+                elif puppetclass["level"]==3:
+                    serverdict["lvl4"]=puppetclass["name"]
+                elif puppetclass["level"]==4:
+                    serverdict["lvl5"]=puppetclass["name"]
+            serverslist.append(serverdict)
+        widget_context = {"servers":serverslist}
         return dict(super_context.items() + widget_context.items())
