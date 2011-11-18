5535ae5cd9be694cadd4813180be40bb11e5574d
Marco Mornati <ilmorna@gmail.com>
2011-11-18 09:13:12 +0100

    Added Start/Stop Weblogic Instance

--- src/webui/platforms/weblogic/operations.py
+++ src/webui/platforms/weblogic/operations.py
@@ -3,7 +3,7 @@ Created on Nov 8, 2011
 
 @author: mmornati
 '''
-from webui.abstracts import ContextOperation
+from webui.abstracts import ContextOperation, ServerOperation
 from webui import settings
 from webui.core import kermit_modules
 
@@ -38,5 +38,70 @@ class WeblogicLogContextMenu(ContextOperation):
         return 'Weblogic'
     
     
+class StartWeblogicInstance(ServerOperation):
+    
+    def get_visible(self, server):
+        agent = server.agents.filter(name='a7xows')
+        return len(agent)==1
+    
+    def get_enabled(self, server):
+        return not server.online
+    
+    def get_name(self):
+        return 'Start Weblogic Instance'
+    
+    def get_image(self):
+        return 'start.png'
+    
+    def request_parameters(self):
+        return True
+    
+    def get_agent(self):
+        return 'a7xows'
+    
+    def get_action(self):
+        return 'startinstance'
+    
+    def get_filter(self, hostname):
+        return 'identity_filter=%s' % hostname
+    
+    def get_url(self, hostname):
+        return None
+    
+class StopWeblogicInstance(ServerOperation):
+    
+    def get_visible(self, server):
+        agent = server.agents.filter(name='a7xows')
+        return len(agent)==1
+    
+    def get_enabled(self, server):
+        return not server.online
+    
+    def get_name(self):
+        return 'Stop Weblogic Instance'
+    
+    def get_image(self):
+        return 'stop.png'
+    
+    def request_parameters(self):
+        return True
+    
+    def get_agent(self):
+        return 'a7xows'
+    
+    def get_action(self):
+        return 'stopinstance'
+    
+    def get_filter(self, hostname):
+        return 'identity_filter=%s' % hostname
+    
+    def get_url(self, hostname):
+        return None
+
+    
+    
 kermit_modules.register(WeblogicDeployContextMenu)
 kermit_modules.register(WeblogicLogContextMenu)
+
+kermit_modules.register(StartWeblogicInstance)
+kermit_modules.register(StopWeblogicInstance)
