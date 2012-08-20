feb11e647cfdf182f03b37205ee122ee6ab219d9
Marco Mornati <ilmorna@gmail.com>
2012-08-20 14:58:20 +0200

    Added test for loaddata on centos5

--- load_data.sh
+++ load_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-python src/webui/manage.py loaddata basedata
-python src/webui/manage.py loaddata widget
+PY=python
+test -f /usr/bin/python26 && PY=python26
+$PY src/webui/manage.py loaddata basedata
+$PY src/webui/manage.py loaddata widget
